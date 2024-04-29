596cb7592c7bc562dc2f3da826b1c8e7ba0137e3
Binod Bhandary <bhandarybinod7@gmail.com>
2024-04-29 23:44:57 +1000

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by iMac on 29/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
