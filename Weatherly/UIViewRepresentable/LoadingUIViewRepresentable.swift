8399b734c30df80d6dc76fc9ecad7128414215b4
Binod Bhandary <bhandarybinod7@gmail.com>
2024-05-04 22:21:34 +1000

    Update loading and tabs in content view

---
+++ Weatherly/UIViewRepresentable/LoadingUIViewRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingUIViewRepresentable.swift
+//  Weatherly
+//
+//  Created by iMac on 4/5/2024.
+//
+
+import SwiftUI
+
+struct LoadingUIViewRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingUIViewRepresentable()
+}
---
+++ Weatherly/View/Partials/AppViewBar.swift
@@ -0,0 +1,18 @@
+//
+//  MyAppBar.swift
+//  Weatherly
+//
+//  Created by iMac on 4/5/2024.
+//
+
+import SwiftUI
+
+struct MyAppBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyAppBar()
+}
