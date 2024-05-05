af0afed623f5b96935b552df8f3928cfcbc41dfe
liudaopeiqi <3157763@gmail.com>
2024-05-05 18:54:10 +1000

    add the comments to Responses

--- Weatherly/Utils/Responses.swift
+++ Weatherly/Utils/Responses.swift
@@ -6,28 +6,29 @@
 //
 
 import Foundation
-
+//Define a struct to decode the json
 struct ResponseBody: Decodable {
-        var coord: CoordinatesResponse
-        var weather: [WeatherResponse]
-        var main: MainResponse
-        var name: String
-        var wind: WindResponse
+        var coord: CoordinatesResponse //hold the geographical coordinates (longitude and latitude)
+        var weather: [WeatherResponse] //an array of weather conditions (could include more than one element for different times of the day
+        var main: MainResponse //main weather data like temperature and humidity
+        var name: String //name of location
+        var wind: WindResponse // wind speed and direction
     struct CoordinatesResponse: Decodable {
             var lon: Double
             var lat: Double
         }
+    //nested struct for decoding weather details from JSON
     struct WeatherResponse: Decodable {
             var id: Double
-            var main: String
+            var main: String // group of weather parameters(Rain, Snow, and others)
             var description: String
-            var icon: String
+            var icon: String //weather icon id
         }
     struct MainResponse: Decodable {
-            var temp: Double
-            var feels_like: Double
-            var temp_min: Double
-            var temp_max: Double
+            var temp: Double // temperature
+            var feels_like: Double //human perception of weather
+            var temp_min: Double // min temperature
+            var temp_max: Double// max temperature
             var pressure: Double
             var humidity: Double
         }
@@ -36,6 +37,7 @@ struct ResponseBody: Decodable {
             var deg: Double
         }
 }
+//extension of MainResponse to provided computed properties for easier access
 extension ResponseBody.MainResponse {
     var feelsLike: Double { return feels_like }
     var tempMin: Double { return temp_min }
