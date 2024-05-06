b73273e5d19b0b19084ff27a4f43b62e67c9992f
liudaopeiqi <3157763@gmail.com>
2024-05-06 20:57:33 +1000

    Add the comments to AppViewBar and CurrentView

--- Weatherly/View/Tabs/CurrentView.swift
+++ Weatherly/View/Tabs/CurrentView.swift
@@ -7,16 +7,18 @@
 
 import SwiftUI
 
-
+//Define a view to display current weather info for a given location
 struct CurrentView: View {
-    var cityInfo: CityInfo?
+    var cityInfo: CityInfo?//optional data containing weather and city details
     
     var body: some View {
+        //Attempt to retrieve the weather info using the current weather code
         let weatherInfo: WeatherInfo? = getWeatherInfo(weather_code: cityInfo?.current?.weather_code ?? -1)
-        
+        //check if current weather data exists
         if (cityInfo?.current != nil) {
             ScrollView {
                 VStack {
+                    //Display the appropriate 3D model based on whether day or night
                     if (weatherInfo != nil) {
                         if (cityInfo?.current?.is_day == 0 && weatherInfo?.nightModel != nil) {
                             BasicLoadingUIViewRepresentable(sceneName: weatherInfo!.nightModel)
@@ -26,6 +28,7 @@ struct CurrentView: View {
                                 .frame(height: 300)
                         }
                     }
+                    //Vertical stack to display temperature and weather description
                     VStack(spacing: 6) {
                         if (cityInfo?.current?.temperature_2m != nil) {
                             Text("\(String(format: "%.0f", cityInfo!.current!.temperature_2m))°")
@@ -41,7 +44,7 @@ struct CurrentView: View {
                         }
                     }
                     //            }
-                    
+                    //subview to display detailed current weather data such as wind speed, humidity and precipitation probability
                     CurrentInfoDash(wind_speed: cityInfo?.current?.wind_speed_10m, humidity: cityInfo?.current?.relative_humidity_2m, precipitation_probability: cityInfo?.current?.precipitation_probability)
                     
                     //            Spacer()
